e6973217146af017eec2e5597ba738d6a2ac5610
Zan Skamljic <zan.skamljic@gmail.com>
2022-08-08 18:48:18 +0200

    refactor: move query functions out

--- retroswift/src/generator/mod.rs
+++ retroswift/src/generator/mod.rs
@@ -9,6 +9,7 @@ use swift_parser::{Definition, Parameter, PostfixModifier};
 use self::errors::GeneratingError;
 
 mod errors;
+mod query;
 #[cfg(test)]
 mod test;
 
@@ -142,7 +143,7 @@ impl Generator {
             definition.path
         ));
         if !definition.query.is_empty() {
-            add_query_parameters(&mut code, definition.query);
+            query::add_parameters(&mut code, definition.query);
         }
         code.add_statement("var request = URLRequest(url: url)")
             .add_statement(&format!(r#"request.httpMethod = "{}""#, definition.verb))
@@ -224,7 +225,7 @@ impl Generator {
         let path = path_parts.next().map(|p| p.to_string()).ok_or_else(|| {
             GeneratingError::GeneralError("Unable to split path and query".into())
         })?;
-        let query_params = parse_query_params(path_parts.next())?;
+        let query_params = query::parse_params(path_parts.next())?;
         Ok(CallDefinition {
             verb,
             path,
@@ -252,56 +253,6 @@ fn make_constructor() -> FunctionBuilder {
         .add_code(code);
 
     constructor
-}
-
-fn parse_query_params(query: Option<&str>) -> Result<Vec<(String, QueryValue)>> {
-    let query = match query {
-        Some(value) => value,
-        None => return Ok(vec![]),
-    };
-    let mut query_values = vec![];
-    for query_item in query.split('&') {
-        let mut parts = query_item.split('=');
-        let name = parts
-            .next()
-            .ok_or_else(|| GeneratingError::GeneralError("Query name required".into()))?
-            .to_string();
-        let value = match parts.next() {
-            Some(value) => {
-                if let Some(suffix) = value.strip_prefix(':') {
-                    QueryValue::Parameter(suffix.into())
-                } else {
-                    QueryValue::Value(value.into())
-                }
-            }
-            None => QueryValue::None,
-        };
-
-        query_values.push((name, value))
-    }
-
-    Ok(query_values)
-}
-
-fn add_query_parameters(code: &mut CodeBuilder, query: Vec<(String, QueryValue)>) {
-    code.add_statement("var urlComponents = URLComponents(string: url.absoluteString)!")
-        .add_statement("var queryItems = urlComponents.queryItems ?? []");
-    query.into_iter().for_each(|(name, value)| {
-        let statement = match value {
-            QueryValue::None => {
-                format!(r#"queryItems.append(URLQueryItem(name: "{name}", value: nil))"#,)
-            }
-            QueryValue::Parameter(parameter) => {
-                format!(r#"queryItems.append(URLQueryItem(name: "{name}", value: {parameter}))"#,)
-            }
-            QueryValue::Value(value) => {
-                format!(r#"queryItems.append(URLQueryItem(name: "{name}", value: "{value}"))"#,)
-            }
-        };
-        code.add_statement(&statement);
-    });
-    code.add_statement("urlComponents.queryItems = queryItems")
-        .add_statement("url = urlComponents.url!");
 }
 
 struct CallDefinition {
